79ea4b6d18d3a0cfef4ca50b50c096c5f23ec664
valentin <mihalache.valentinflorian@gmail.com>
2025-06-28 18:30:57 +0300

    POM User Class

--- tests/pages/userCreate.page.ts
+++ tests/pages/userCreate.page.ts
@@ -1,13 +1,35 @@
 import {type Locator, type Page} from '@playwright/test'
-interface Locators {
-    page: Page
-    Element: Locator
-}
 
-export class CrateUser {
-    readonly page: Locators['page']
-    readonly signUpButton: Locators['Element']
-    readonly userName: Locators['Element']
-    readonly password: Locators['Element']
-    readonly closeButton: Locators['Element']
+
+export class CreateUser {
+    readonly page: Page
+    readonly openSignUpModalButton: Locator
+    readonly userName: Locator
+    readonly password: Locator
+    readonly closeButton: Locator
+    readonly signUpButton: Locator
+
+constructor (page: Page) {
+this.page = page
+this.openSignUpModalButton = page.getByTestId("#signInModal")
+this.userName = page.getByRole("textbox",{name:"UserName"})
+this.password = page.getByRole("textbox",{name:"Password"})
+this.signUpButton=page.getByRole("button", {name:"Sign Up"})
+this.closeButton=page.getByRole("button", {name:"Close"})
+}
+async goto () {
+    await this.page.goto("https://www.demoblaze.com/index.html")
+}
+async openCreateUserFlow () {
+    await this.openSignUpModalButton.click()
+}
+async fillUserNameBox (fixtureName: string) {
+    await this.userName.fill(fixtureName)
+}
+async fillPasswordBox(fixturePassword: string) {
+    await this.password.fill(fixturePassword)
+}
+async signUp() {
+    await this.signUpButton.click()
+}
 }
